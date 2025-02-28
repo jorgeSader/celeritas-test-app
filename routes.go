7d60825226f0e24e686d889e874bb9e19e659bf1
JSader <jorge.sader@gmail.com>
2025-02-28 11:03:20 +0100

    chore: try out the rendering functionality of Jet Templates with actual jet template

--- routes.go
+++ routes.go
@@ -18,9 +18,6 @@ func (a *application) routes() *chi.Mux {
 
 	// add routes here
 	a.App.Routes.Get("/", a.Handlers.Home)
-	a.App.Routes.Get("/jet", func(w http.ResponseWriter, r *http.Request) {
-		a.App.Render.JetPage(w, r, "test-jet", nil, nil)
-	})
 
 	//static routes
 	fileServer := http.FileServer(http.Dir("./public/"))
